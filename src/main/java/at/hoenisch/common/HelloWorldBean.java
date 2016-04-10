50439ddbaf6d201bcbdd088e4685aa99fec709bc
Philipp Hoenisch <philipp.hoenisch@senacor.com>
2016-04-10 17:42:29 +0200

    hello world init

---
+++ src/main/java/at/hoenisch/common/HelloWorldBean.java
@@ -0,0 +1,40 @@
+package at.hoenisch.common;
+
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import java.io.Serializable;
+
+/**
+ * Created by Philipp Hoenisch, Senacor on 09/04/16.
+ */
+@ManagedBean
+@SessionScoped
+public class HelloWorldBean implements Serializable{
+
+    private String firstname;
+    private String lastname;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void save() {
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage("Welcome " + firstname + " " + lastname));
+    }
+}
